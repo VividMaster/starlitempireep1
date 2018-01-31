28ba3f50a18628f9ed9f0a3f914768ed6d4c9757
Anthony Robert Wells <scopedynamo@gmail.com>
2018-01-31 20:46:25 +0000

    added basic editor project.
    commit 2.

---
+++ StarlitEmpire/obj/Debug/android/src/md5c2a739b7e95e587e73e1e7528c62c26a/MainActivity.java
@@ -0,0 +1,65 @@
+package md5c2a739b7e95e587e73e1e7528c62c26a;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"";
+		mono.android.Runtime.register ("StarlitEmpire.MainActivity, StarlitEmpire, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("StarlitEmpire.MainActivity, StarlitEmpire, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
