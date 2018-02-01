f5ab9489bf67cdfd3d348f7ad1c24ccc56fd5ea4
Anthony Robert Wells <scopedynamo@gmail.com>
2018-02-01 01:31:33 +0000

    more work on the editor
    more additions/changes.

---
+++ StarData/StarData/StarState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarData
+{
+    public enum StateType
+    {
+      Map,Battle,Cine,Menu,Intro,None
+    }
+    public enum ActionType
+    {
+        PlaySound,PlayMusic,DisplayImage,DisplayString,DisplayAnim,DisplayFMV,None
+    }
+    public class StarCol
+    {
+        public int R=255, G=255, B=255, A=255;
+        public StarCol(int r,int g,int b,int a = 255)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+    }
+    public class StarAction
+    {
+        public ActionType Type = ActionType.None;
+        public string DataPath = "";
+        public string SubDataPAth = "";
+        public int X=0, Y=0, Z=0;
+        public 
+    }
+    public class StarState
+    {
+        public StateType Type = StateType.None;
+        public string Name = "";
+    }
+}
---
+++ StarEngine/StarEngine/StarSys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarEngine
+{
+    public class StarSys
+    {
+        public StarRender Render = null;
+        public StarIO IO = null;
+        public StarInput Input = null;
+    }
+}
